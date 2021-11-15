4ada71561a3a0c52c24e556c8762701a0f8f4e4a
StivenVB <stiven.1701722520@ucaldas.edu.co>
2021-11-14 20:18:45 -0500

    Configuración, grafo1 y grafos de prueba

---
+++ PrimeraEntrega/grafos/grafo_inicial_con_cm.py
@@ -0,0 +1,35 @@
+import os
+import pyphi
+import numpy as np
+
+pyphi.config.load_file('../config/pyphi_config.yml')
+
+tpm = np.array([
+[0, 0, 0],
+[0, 0, 1],
+[1, 0, 1],
+[1, 0, 0],
+[1, 1, 0],
+[1, 1, 1],
+[1, 1, 1],
+[1, 1, 0]
+])
+
+cm = np.array([
+[0, 0, 1],
+[1, 0, 1],
+[1, 1, 0]
+])
+
+#distance_emd = pyphi.distance.emd(tpm,cm)
+#distance_kmd = pyphi.distance.emd(tpm,cm)
+
+labels = ('A', 'B', 'C')
+network = pyphi.Network(tpm, cm=cm, node_labels=labels)
+state = (1, 0, 0)
+node_indices = (0, 1, 2)
+subsystem = pyphi.Subsystem(network, state, node_indices)
+sia = pyphi.compute.sia(subsystem)
+print("MIP: \n", sia.cut)
+print("Phi: \n Φ = ", sia.phi)
+print("Time: \n", sia.time, "s")
