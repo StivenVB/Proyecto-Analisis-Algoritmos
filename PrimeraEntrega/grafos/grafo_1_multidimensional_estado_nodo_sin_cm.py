2baae9a1e47415ee80e0da90a1e5c5664a297bd5
StivenVB <stiven.1701722520@ucaldas.edu.co>
2021-11-15 04:05:44 -0500

    Grafos: 3, 5, 6, 7, 8, , JSON configuracion

--- PrimeraEntrega/grafos/grafo_1_multidimensional_estado_nodo_sin_cm.py
+++ PrimeraEntrega/grafos/grafo_1_multidimensional_estado_nodo_sin_cm.py
@@ -5,21 +5,25 @@ import numpy as np
 pyphi.config.load_file('../config/pyphi_config.yml')
 
 tpm = np.array([
-[0, 0, 0],
-[1, 0, 0],
-[1, 0, 1],
-[0, 0, 1],
-[0, 0, 1],
-[1, 1, 1],
-[1, 0, 0],
-[0, 1, 0]
+[[[0.0, 0.0, 0.0],
+  [0.0, 0.0, 1.0]],
+
+  [[1.0, 0.0, 1.0],
+  [1.0, 0.0, 0.0]]],
+
+[[[1.0, 0.0, 0.0],
+  [1.0, 1.0, 1.0]],
+
+  [[0.0, 0.0, 1.0],
+  [0.0, 1.0, 0.0]]]
 ])
+
 
 #distance_emd = pyphi.distance.emd(tpm,cm)
 #distance_kmd = pyphi.distance.emd(tpm,cm)
 
 labels = ('A', 'B', 'C')
-network = pyphi.Network(tpm, node_labels=labels)
+network = pyphi.Network(tpm, cm=cm, node_labels=labels)
 state = (1, 0, 0)
 node_indices = (0, 1, 2)
 subsystem = pyphi.Subsystem(network, state, node_indices)
